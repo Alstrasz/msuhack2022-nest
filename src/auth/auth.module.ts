5be611a903f18f81e56d5d422ee307914f785b10
Alstrasz <lutsai2000@ya.ru>
2022-04-10 18:13:11 +0300

    porloned token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './jwt.strategy';
         PassportModule,
         JwtModule.register( {
             secret: jwt_constants.secret,
-            signOptions: { expiresIn: '1h' },
+            signOptions: { expiresIn: '1d' },
         } ),
     ],
     providers: [
